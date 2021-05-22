832fd96b19198b2cceb3ed04d79d6c393070818b
Elmurod Nurtaev <e.a.nurtaev@gmail.com>
2021-05-22 18:49:48 +0500

    fixed naming in model Messages

--- src/main/java/main/model/Message.java
+++ src/main/java/main/model/Message.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
-@Entity (name = "Messages")
+@Entity
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
